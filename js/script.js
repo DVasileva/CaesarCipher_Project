99447b8a85b02bc72705d28e08ac0e5062dbc778
DVasileva <74765255+DVasileva@users.noreply.github.com>
2021-07-13 14:23:58 +0300

    index_of

--- js/script.js
+++ js/script.js
@@ -1,35 +1,31 @@
-	 //other option ???
-		var letters = 'абвгдежзийклмнопрстуфхцчшщъьюя';
-		allLet = letters + letters + letters;
+var letters = 'абвгдежзийклмнопрстуфхцчшщъьюя';
+	allLet = letters + letters + letters;
 
-
-		function encrypt(){
+	function encrypt(){
 			var inputMsg = $('#original').val();
 			var inputShift = $('#offset').val();
 
 			inputShift = (inputShift % letters.length);
+			
 			var encryptMsg = '';
 
 			for ( i = 0; i < inputMsg.length; i ++ ){
 				var currentLetter = inputMsg[i];
 				var upperCase = (currentLetter == currentLetter.toUpperCase());
-				//console.log(upperCase);
-				letter = currentLetter.toLowerCase();
+		
+				currentLetter = currentLetter.toLowerCase();
 
 
-				var ind = letters.indexOf(currentLetter); //-> returns index at an element can be found 
+				var ind = letters.indexOf(currentLetter); //position of the first occurrence of
+														  // a specified value in a string.
 
 					if(ind == -1 ){
 						encryptMsg += currentLetter;
-							//console.log(encryptMsg);
+						
 					} else {
 						ind = ((ind + inputShift) + letters.length);
-
-						 var newLetter = allLet[ind];
-						 if(upperCase)
-						 	newLetter = newLetter.toUpperCase();
-						 		encryptMsg += newLetter;
-
+						console.log(ind);
+						
 					}
 
 			}
@@ -42,7 +38,7 @@
 			let inputMsg = $('#original')[0];
 			let originalMsg = inputMsg.value;
 
-			inputMsg.addEventListener('input', enteredLetter, false)
+			inputMsg.addEventListener('input', enteredLetter)
 
 			function enteredLetter(e){
 				originalMsg = e.target.value;
