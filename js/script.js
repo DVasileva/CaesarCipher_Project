66ffc95ed5f2a95e197795fdc04e194e9f900dcc
DVasileva <74765255+DVasileva@users.noreply.github.com>
2021-07-14 13:13:24 +0300

    line

--- js/script.js
+++ js/script.js
@@ -1,56 +1,54 @@
 var letters = 'абвгдежзийклмнопрстуфхцчшщъьюя';
-	allLet = letters + letters + letters;
+allLet = letters + letters + letters;
 
-	function encrypt(){
-			var inputMsg = $('#original').val();
-			var inputShift = $('#offset').val();
+function encrypt(){
+var inputMsg = $('#original').val();
+var inputShift = $('#offset').val();
 
-			inputShift = (inputShift % letters.length);
-			
-			var encryptMsg = '';
+inputShift = (inputShift % letters.length);
 
-			for ( i = 0; i < inputMsg.length; i ++ ){
-				var currentLetter = inputMsg[i];
-				var upperCase = (currentLetter == currentLetter.toUpperCase());
-		
-				currentLetter = currentLetter.toLowerCase();
+var encryptMsg = '';
+
+for ( i = 0; i < inputMsg.length; i ++ ){
+var currentLetter = inputMsg[i];
+var upperCase = (currentLetter == currentLetter.toUpperCase());
+
+currentLetter = currentLetter.toLowerCase();
 
 
-				var ind = letters.indexOf(currentLetter); //position of the first occurrence of
-														  // a specified value in a string.
+		var ind = letters.indexOf(currentLetter); 
 
-					if(ind == -1 ){
-						encryptMsg += currentLetter;
-						
-					} else {
+				if(ind == -1 ){
+					encryptMsg += currentLetter;
+
+				} else {
+					
 						ind = ((ind + inputShift) + letters.length);
-						//console.log(ind);
-						  var newLetter = allLet[ind];
-						 if(upperCase)
-						 	newLetter = newLetter.toUpperCase();
-						  		encryptMsg += newLetter;
-
-					}
-
+				//console.log(ind);
+				var newLetter = allLet[ind];
+				if(upperCase)
+					newLetter = newLetter.toUpperCase();
+				encryptMsg += newLetter;
 			}
-			$('#cipher').val(encryptMsg);
 
 		}
+		$('#cipher').val(encryptMsg);
+
+	}
+
+	$(document).ready(function(){
+		let inputMsg = $('#original')[0];
+		let originalMsg = inputMsg.value;
+
+		inputMsg.addEventListener('input', enteredLetter)
+
+		function enteredLetter(e){
+			originalMsg = e.target.value;
+			originalMsg = originalMsg.replace( /[^а-я, А-Я\s]/, '' );
+			e.target.value = originalMsg;
+			encrypt();
+		}
+
+	});
 
 
-		$(document).ready(function(){
-			let inputMsg = $('#original')[0];
-			let originalMsg = inputMsg.value;
-
-			inputMsg.addEventListener('input', enteredLetter)
-
-			function enteredLetter(e){
-				originalMsg = e.target.value;
-				originalMsg = originalMsg.replace( /[^а-я, А-Я\s]/, '' );
-				 e.target.value = originalMsg;
-				encrypt();
-			}
-
-		});
-
-		
